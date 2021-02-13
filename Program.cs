862f9d68dfca5f99757de500e0f05988146a4dd8
Jorge Angel Chavez Guzman <kokito83012@hotmail.com>
2021-02-13 16:28:15 -0600

    Intento arreglo bidimensional

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MatrixDeterminant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int[,] array = new int [3, 3] { {1, 3, 5}, {2, 4, 6}, {3, 5, 9} };
+            int suma ;
+            int i, j;
+
+            suma = 0;
+
+            for ( i = 0; i < 3; i++ )
+                for ( j = 0; j < 3; j++ )
+                    suma = suma + array[i, j];
+            Console.WriteLine("inserta numero", suma );
+
+            
+            
+        }
+    }
+}
